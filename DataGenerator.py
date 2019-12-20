712e77430861328a1e6cebc371eb9637a7d31b9d
Loris Pilotto <loris.pilotto@epfl.ch>
2019-12-20 13:45:12 +0100

    modify 2 files

--- DataGenerator.py
+++ DataGenerator.py
@@ -3,12 +3,14 @@
 
 import keras
 import numpy as np
+from keras.preprocessing.image import ImageDataGenerator
+from scipy.stats import bernoulli
+
 
 class DataGenerator(keras.utils.Sequence):
     """Generates Data for Keras"""
     
-   
-    def __init__(self, list_IDs, batch_size=32, dim=(512,32,35),n_channels=2, n_classes = 3, shuffle=True):
+    def __init__(self, list_IDs, frames_dir, masks_dir, batch_size=32, dim=(352,512,35),n_channels=2, n_classes = 3, shuffle=True):
         
         """Initialize Generator"""
         
@@ -18,8 +20,25 @@ class DataGenerator(keras.utils.Sequence):
         self.n_channels = n_channels
         self.n_classes = n_classes
         self.shuffle = shuffle
-        self.on_epoch_end() 
+        self.on_epoch_end()
+        self.frames_dir = frames_dir
+        self.masks_dir = masks_dir
         
+    
+    def augmentation_params(self, shift_range=0, rotate_range=0, zoom_range=0, augment=False, normalize=False, aug_prob = 0.5):
+        """--shift_range = a, a is fraction between (0,1): causes shift in x between (-a*dim_x,a*dim_x), similarly in y
+           --rotate_range = a, a is in degrees: causes rotation in degrees between (-rotate_range, rotate_range)
+           --zoom = a, a is a fraction between (0,1): causes zoom between (1-a) to (1+a)
+           --augment = a, a is binary True or False: setting false means no augmentation
+           --normalize = a, a is binary True or False: setting false means no normalization(turned off for masks automatically)"""
+        
+        self.shift = shift_range
+        self.rotate = rotate_range
+        self.zoom = zoom_range
+        self.augment = augment
+        self.normalize = normalize
+        self.aug_prob = aug_prob
+    
     def __len__(self):
         return int(np.floor(len(self.list_IDs)/self.batch_size))
     
@@ -55,30 +74,94 @@ class DataGenerator(keras.utils.Sequence):
             
             for i, ID in enumerate(list_IDs_temp):
             # Store sample
-                X[i, :, :, :, 0] = np.load('/content/drive/My Drive/gitRendu/data/frames/frame_' + str(ID) + '.npy')
+
+                seed = np.random.randint(10000, size=1)[0]
+                temp_x = np.load(self.frames_dir + '/frame_' + str(ID) + '.npy')
+                temp_x = augmentation(temp_x, self.shift, self.rotate, self.zoom,self.augment, self.normalize, self.aug_prob, seed)
+                X[i, :, :, :, 0] = temp_x
 
                 # Store class
-                y[i, :, :, :, 0] = np.load('/content/drive/My Drive/gitRendu/data/masks/mask_' + str(ID) + '.npy')
+                
+                temp_y = np.load(self.masks_dir + '/mask_' + str(ID) + '.npy')
+                temp_y = augmentation(temp_y, self.shift, self.rotate, self.zoom,self.augment, False, self.aug_prob, seed)
+                y[i, :, :, :, 0] = temp_y
 
         else:
             
-
             # Generate data
             for i, ID in enumerate(list_IDs_temp):
                 # Store sample - Normalizing(Try without it also)
-                
-                X[i] = np.load('/content/drive/My Drive/gitRendu/data/frames/frame_' + str(ID) + '.npy')/255
+                seed = np.random.randint(10000, size=1)[0]
+
+                temp_x = np.load(self.frames_dir + '/frame_' + str(ID) + '.npy')
+                temp_x = augmentation(temp_x, self.shift, self.rotate, self.zoom, self.augment, self.normalize, self.aug_prob, seed)
+                X[i] = temp_x
                 
                 
 
                 # Store class
-                y[i] = np.load('/content/drive/My Drive/gitRendu/data/masks/mask_' + str(ID) + '.npy')
+                temp_y = np.load(self.masks_dir + '/mask_' + str(ID) + '.npy')
+                temp_y = augmentation(temp_y, self.shift, self.rotate, self.zoom, self.augment, False, self.aug_prob, seed)
+                y[i] = temp_y
 
         return X, keras.utils.to_categorical(y, num_classes=self.n_classes)
 
+    
+def augmentation(x, shift_range, rotate_range, zoom_range, augment=False,normalize=False, aug_prob = 0.5, seed = 1):
+    
+    """FEED IMAGE AS (X,Y,Z) 
+       AUGMENTATION IN (X,Y) DIMS"""
+    
+    x_transformed = x
+    
+    if(augment == True):
+        np.random.seed(seed)
+        r = bernoulli.rvs(aug_prob, size=1)
+        
+        if(r[0] == 1):
+
+          image1_datagen = ImageDataGenerator()
+
+          shift = np.linspace(-1*shift_range, 1*shift_range, 101)
+          
+          index_x = np.random.choice(shift.shape[0], 1, replace=True)
+          shift_x = int(shift[index_x] * x.shape[0])
+          index_y = np.random.choice(shift.shape[0], 1, replace=True)
+          shift_y = int(shift[index_y] * x.shape[1])
+          
+          rotate = np.linspace(-1*rotate_range, 1*rotate_range, 2*rotate_range+1)
+          
+          index_r = np.random.choice(rotate.shape[0], 1, replace=True)
+          deg = int(rotate[index_r])
+          
+          zoom = np.linspace(1-zoom_range, 1+zoom_range, 101)
+          
+          index_zx = np.random.choice(zoom.shape[0], 1, replace=True)
+          shift_zx = zoom[index_x][0]
+
+          index_zy = np.random.choice(zoom.shape[0], 1, replace=True)
+          shift_zy = zoom[index_y][0]
+          
+          data_gen_args = dict(theta=deg,
+                            tx=shift_x,
+                            ty=shift_y,
+                            zx=shift_zx,
+                            zy=shift_zy)
+          
+          #print(data_gen_args)
+          
+          x_transformed = image1_datagen.apply_transform(x, transform_parameters=data_gen_args)
+    
+    if(normalize == True):
+        min_val = np.min(x_transformed)
+        max_val = np.max(x_transformed)
+        if(max_val != min_val):
+            x_transformed = (x_transformed - min_val)/(max_val - min_val+0.00001)
+        
+    return x_transformed
+    
 
 # In[ ]:
-
 
 
 
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Identification of "AIA terminal nodule" and "AIA central nodule" using DenseNetFCN-3D.
 
-Based on two different 3D movies (`20190805_SJR3.2.2_w1_s2.nd2` and `20190805_322_w1_s2.nd2`), provided by the [Laboratory of the Physics of Biological Systems](https://www.epfl.ch/labs/lpbs/), our task is to identify and distinguish two parts (the **terminal nodule** and the **central nodule**) of a neuron type called AIA.  
+Based on two different 3D movies (`20190805_SJR3.2.2_w1_s2.nd2` and `20190805_322_w1_s2.nd2`), provided by the [Laboratory of the Physics of Biological Systems](https://www.epfl.ch/labs/lpbs/), our task is to identify and distinguish two parts (the *terminal nodule* and the *central nodule*) of a neuron type called AIA.  
 For  our  work  we  considered  all  frames  as different  volumes,  not  taking  into  account  time  continuity.  
 We used a DenseNetFCN-3D with 4 dense blocks of 3 layers per block (mostly because of computation constraints).  
 
@@ -18,7 +18,7 @@ We used a DenseNetFCN-3D with 4 dense blocks of 3 layers per block (mostly becau
 ### How to train the DenseNetFCN-3D, predict masks and note the model:
 We executed the code on Google Colab by following these steps:
 1. The training samples and their corresponding masks need to be stored as `.npy` files. Their names should be `frame_i.npy`and `mask_i.npy` respectively (for i going from 0 → maxTime) and frames/masks need to be stored in two different folders. If you gives the good PATHs in the second cell of `generateFramesMasksFromVideo.ipynb`, this notebook does this steps for you.  
-P.S: To help our model, we labeled **ALL** cell parts that were not AIA central/terminal noduls with the same value. Thus, the model only has to distinguish 3 types of cells.
+P.S: To help our model, we labeled *ALL* cell parts that were not AIA central/terminal noduls with the same value. Thus, the model only has to distinguish 3 types of cells.
 2. Give the wanted paths and variables values to all the second cell's variables of `Training.ipynb` notebook. Then execute all cells. It will train a DenseNetFCN-3D and save the model.
 3. Once the model has been trained, it is possible to have a visual idea of the prediction with the `VisualizationPrediction.ipynb` notebook. First, change the variables/paths of the second cell and then run all the cells. The last cell shows the image, the mask and finally the predicted mask.
 4. Finally, the `ConfusionMatrix.ipynb` notebook can be used (once the PATHs from cell 2 have been changed) to compute the accuracy and the F1 score for the 'AIA terminal nodule' (class 1) , the 'AIA central nodule'(class 2) and the other cells (class 0).
